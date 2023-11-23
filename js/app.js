0b406ec28b136a82a8b9c24a34190a5c5aee11ad
giusmili <giusmili67@gmail.com>
2023-11-23 12:45:01 +0100

    darkmode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /* object date */
 import { Data_user } from "./composant.js";
+import { icone } from "./composant.js";
+import { btn } from "./composant.js";
+
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
     Data_user.formatDateTime();
+
+    btn.event_css.addEventListener("click", () => {
+        btn.events();
+    });
+
+    icone(document.querySelector("h1"));
 });
